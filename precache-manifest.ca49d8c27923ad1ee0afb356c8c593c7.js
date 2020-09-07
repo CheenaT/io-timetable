c229d03dacc5b28c391b17920d7e57684598081b
Mac <mac@Macs-MacBook-Air.local>
2020-09-07 23:57:51 +0300

    Updates

---
+++ precache-manifest.ca49d8c27923ad1ee0afb356c8c593c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04c1f14a94763e3da7aee39489bd98e",
+    "url": "/actuarial-schedule/index.html"
+  },
+  {
+    "revision": "059c871d6e364c826435",
+    "url": "/actuarial-schedule/static/css/main.f785bc78.chunk.css"
+  },
+  {
+    "revision": "705ede07cb156fe50c6f",
+    "url": "/actuarial-schedule/static/js/2.6f1f0be4.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/actuarial-schedule/static/js/2.6f1f0be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059c871d6e364c826435",
+    "url": "/actuarial-schedule/static/js/main.ecd7b30d.chunk.js"
+  },
+  {
+    "revision": "6dbd5f8261ac66803674",
+    "url": "/actuarial-schedule/static/js/runtime-main.225885fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actuarial-schedule/static/media/logo.5d5d9eef.svg"
+  }
+]);
